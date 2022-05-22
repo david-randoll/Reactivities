b4829393244f0602644c60db398f5ba0f4cd24c0
d-randoll <51640895+d-randoll@users.noreply.github.com>
2022-05-22 19:01:33 -0400

    Added security headers

--- API/Startup.cs
+++ API/Startup.cs
@@ -61,7 +61,12 @@ namespace API
             }
             else
             {
-                app.UseHsts();
+                // app.UseHsts();
+                app.Use(async (context, next) =>
+                {
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+                    await next.Invoke();
+                });
             }
 
             // app.UseHttpsRedirection();
